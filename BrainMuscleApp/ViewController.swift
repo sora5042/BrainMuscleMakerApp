a6786540634526d629d9542ea4faf0f10fdf8faa
ooya sora <sora-110507@ezweb.ne.jp>
2021-01-28 11:42:29 +0900

    Initial Commit

---
+++ BrainMuscleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrainMuscleApp
+//
+//  Created by 大谷空 on 2021/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
